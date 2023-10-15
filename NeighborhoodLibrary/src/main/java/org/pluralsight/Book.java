b5bd26e2b4c964e58e76e728a569a3adc86e9083
Christian Escano <escanochristian21@gmail.com>
2023-10-14 21:30:14 -0400

    Added: class instance variables, methods, getters/setters

--- NeighborhoodLibrary/src/main/java/org/pluralsight/Book.java
+++ NeighborhoodLibrary/src/main/java/org/pluralsight/Book.java
@@ -1,4 +1,62 @@
 package org.pluralsight;
 
 public class Book {
+
+    private int id;
+    private boolean isCheckedOut;
+    private String isbn;
+    private String title;
+    private String checkedOutTo;
+
+
+
+    /*-----Functions-----*/
+    public void checkOut(String name) {
+
+    }
+
+    public void checkIn() {
+
+    }
+
+    /*-----Getters/Setters-----*/
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
 }
