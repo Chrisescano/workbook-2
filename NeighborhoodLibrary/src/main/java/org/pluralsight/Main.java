dce3b4ca6ac67e41d048306ecf4600df6794ce4e
Christian Escano <escanochristian21@gmail.com>
2023-10-14 21:53:33 -0400

    Added: home screen logic, user input, and quit program functionality

--- NeighborhoodLibrary/src/main/java/org/pluralsight/Main.java
+++ NeighborhoodLibrary/src/main/java/org/pluralsight/Main.java
@@ -1,6 +1,43 @@
 package org.pluralsight;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String homeScreenPrompt = "Here is what you can do:\n  (A) - Shows available books\n  (C) - Shows checked out books\n" +
+                "  (Q) - Quit the program\nType in your command: ";
 
+        Scanner scanner = new Scanner(System.in);
+        Book[] libraryCollection = new Book[20];
+
+        boolean isDone = false;
+
+        //Store Home Screen
+        System.out.println("Welcome to the Pluralsight library!");
+
+        while (!isDone) {
+            char command = getCharInput(scanner, homeScreenPrompt);
+            switch (command) {
+                case 'A':
+                    System.out.println("Show available books...");
+                    break;
+                case 'C':
+                    System.out.println("Show checked out books...");
+                    break;
+                case 'Q':
+                    System.out.println("Quitting the program");
+                    isDone = true;
+                    break;
+                default:
+                    System.out.println("Sorry, that is not a valid command - type: A, C, or Q");
+            }
+        }
+    }
+
+    public static char getCharInput(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        char input = scanner.next().toUpperCase().charAt(0);
+        scanner.nextLine(); //clears the buffer
+        return input;
     }
 }
